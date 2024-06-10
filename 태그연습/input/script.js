d864ca0764b56945bd886aa1db47afc539946140
lkm622 <mombulsala@gmail.com>
2024-06-10 10:20:22 +0900

    ✨ 회원가입

--- 태그연습/input/script.js
+++ 태그연습/input/script.js
@@ -1,25 +1,23 @@
-let year = document.querySelector('.year');
+let year = document.getElementById('year');
+let month = document.getElementById('month');
+let date = document.getElementById('date');
 
-//한국 표준시 (Korea Standard Time)
 let kst = new Date();
-let getYear = kst.getFullYear();  //년도값만
-let getMonth = kst.getMonth();
-let getDate = kst.getDate();
-//console.log(getMonth);
+let getYear = kst.getFullYear();
 
-
-
-for(let i = 1920; i <= 2024; i++) {
-  // let options = document.createElement('option');
-  // options.innerHTML = i;
-  // year.appendChild(options);
-
-  let options = `<option>${i}</option>`;
-  // afterbegin, beforebegin, afterend, beforeend
-  // 추가하고 싶은곳.insertAdjacentHTML('위치', 추가할 요소);
-  year.insertAdjacentHTML('beforeend', options);
-
-  // // jQuery
-  // $('.year').append('<option>'+ i +'</option>');
+for(let i = 1920; i <= getYear; i++ ){
+  let option = `<option>${i}</option>`;
+  year.insertAdjacentHTML('beforeend', option);
 }
+
+for(let i = 1; i <= 12; i++ ){
+  let option = `<option>${i}</option>`;
+  month.insertAdjacentHTML('beforeend', option);
+}
+
+for(let i = 1; i <= 31; i++ ){
+  let option = `<option>${i}</option>`;
+  date.insertAdjacentHTML('beforeend', option);
+}
+
 
